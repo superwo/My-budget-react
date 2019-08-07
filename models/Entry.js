3f3d5612163a52f638e16289bd3bbe51855326b7
Radu <avadanii@tiscali.it>
2019-08-07 09:25:55 +0300

    Entry model and entries routes

---
+++ models/Entry.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const EntrySchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: String,
+    default: 'expense'
+  },
+  amount: {
+    type: Number,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('entry', EntrySchema);
--- routes/entries.js
+++ routes/entries.js
@@ -1,20 +1,69 @@
 const express = require('express');
-
 const router = express.Router();
+const auth = require('../middleware/auth');
+const { check, validationResult } = require('express-validator/check');
+
+const User = require('../models/User');
+const Entry = require('../models/Entry');
 
 // @route GET api/entries
 // @desc Get all users entries
 // @access Private
-router.get('/', (req, res) => {
-  res.send('Get all entries');
+router.get('/', auth, async (req, res) => {
+  try {
+    const entries = await Entry.find({ user: req.user.id }).sort({ date: -1 });
+    res.json(entries);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 // @route POST api/entries
 // @desc Add new entry
 // @access Private
-router.post('/', (req, res) => {
-  res.send('Add entry');
-});
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('name', 'Name is required')
+        .not()
+        .isEmpty(),
+      check('category', 'Category is required')
+        .not()
+        .isEmpty(),
+      check('amount', 'Enter a valid amount')
+        .not()
+        .isEmpty()
+        .isNumeric()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, category, type, amount } = req.body;
+
+    try {
+      const newEntry = new Entry({
+        name,
+        category,
+        type,
+        amount,
+        user: req.user.id
+      });
+
+      const entry = await newEntry.save();
+      res.json(entry);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 // @route PUT api/entries/:id
 // @desc Update entry
