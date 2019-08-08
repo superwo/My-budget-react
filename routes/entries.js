21e53f46e3fa3ff4d484af809cf5d32bd9e3b5b4
Radu <avadanii@tiscali.it>
2019-08-08 08:52:00 +0300

    Update and Delete entry routes

--- routes/entries.js
+++ routes/entries.js
@@ -68,15 +68,60 @@ router.post(
 // @route PUT api/entries/:id
 // @desc Update entry
 // @access Private
-router.put('/:id', (req, res) => {
-  res.send('Update entry');
+router.put('/:id', auth, async (req, res) => {
+  const { name, category, type, amount } = req.body;
+
+  // Build entry object
+  const entryFields = {};
+  if (name) entryFields.name = name;
+  if (category) entryFields.category = category;
+  if (type) entryFields.type = type;
+  if (amount) entryFields.amount = amount;
+
+  try {
+    let entry = await Entry.findById(req.params.id);
+
+    if (!entry) return res.status(404).json({ msg: 'Entry not found' });
+
+    // Make sure user owns entry
+    if (entry.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'Not authorized' });
+    }
+
+    entry = await Entry.findByIdAndUpdate(
+      req.params.id,
+      { $set: entryFields },
+      { new: true }
+    );
+
+    res.json(entry);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 // @route DELETE api/entries/:id
 // @desc Delete entry
 // @access Private
-router.delete('/:id', (req, res) => {
-  res.send('Delete entry');
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    let entry = await Entry.findById(req.params.id);
+
+    if (!entry) return res.status(404).json({ msg: 'Entry not found' });
+
+    // Make sure user owns entry
+    if (entry.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'Not authorized' });
+    }
+
+    await Entry.findByIdAndRemove(req.params.id);
+
+    res.json({ msg: 'Entry removed' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 module.exports = router;
