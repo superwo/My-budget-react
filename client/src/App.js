ebd740e346d844ec2a2176870a4e924a8c0397e6
Radu <avadanii@tiscali.it>
2019-08-17 09:00:10 +0300

    Login and Register components

--- client/src/App.js
+++ client/src/App.js
@@ -3,26 +3,33 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home/Home';
 import About from './components/pages/About/About';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 import EntryState from './context/entry/EntryState';
+import AuthState from './context/auth/AuthState';
 
 import './App.css';
 
 const App = () => {
   return (
-    <EntryState>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <div className='container'>
-            <Switch>
-              <Route exact path='/' component={Home} />
-              <Route exact path='/about' component={About} />
-            </Switch>
-          </div>
-        </Fragment>
-      </Router>
-    </EntryState>
+    <AuthState>
+      <EntryState>
+        <Router>
+          <Fragment>
+            <Navbar />
+            <div className='container'>
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/about' component={About} />
+                <Route exact path='/register' component={Register} />
+                <Route exact path='/login' component={Login} />
+              </Switch>
+            </div>
+          </Fragment>
+        </Router>
+      </EntryState>
+    </AuthState>
   );
 };
 
